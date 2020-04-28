8e243851b38a220d7309999775350d76e14bfa92
Danila Molodtsov <danmol04@gmail.com>
2020-04-28 03:06:57 +0300

    ive broke up something, will try to fix later

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -7,10 +7,39 @@ export class Countdown extends React.Component {
         super(props)
         this.state = {
             disabled: false,
+            timerTime: {
+                secs: 0,
+                mins: 0,
+            },
         }
     }
+
+    sliderHolder = (value) => {
+        // console.log(value);
+        const mins = Math.floor(value/60)
+        const secs = value/60 > 0 ? value%60 : 0
+        this.setState({
+            timerTime: {
+                secs, mins
+            },
+        })
+    }
+
+    minsHolder = ({target: {value}}) => {
+        
+        this.setState({ timerTime: { mins: value } });
+        console.log(this.state.timerTime)
+    }
+
+    secsHolder = ({target: {value}}) => {
+        
+        this.setState({ timerTime: { secs: value } });
+    }
+
+
+
     render() {
-        const { disabled } = this.state;
+        const { disabled, timerTime } = this.state;
         return(
             <div className="countdown">
                 <Slider defaultValue={30}
@@ -18,20 +47,26 @@ export class Countdown extends React.Component {
                 step={15}
                 min={0}
                 max={3600}
+                value={timerTime ? timerTime.secs+ timerTime.mins*60 : null}
+                onChange={this.sliderHolder}
                 />
                 <div className="group">
                     <label>
+                        Minutes
                         <Input type="number"
+                        name="mins"
                         disabled={disabled}
-                        max={43200}
                         placeholder="munutes"
+                        onChange={this.minsHolder}
                         />
                     </label>
                     <label>
+                        seconds
                         <Input type="number"
+                        name="secs"
                         disabled={disabled}
-                        max={720}
                         placeholder="seconds"
+                        onChange={this.secsHolder}
                         />
                     </label>
                 </div>
