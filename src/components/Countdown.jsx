625b998d524db8a925c4788ab6f9a801e7107f44
Danila Molodtsov <danmol04@gmail.com>
2020-04-29 16:47:24 +0300

    few better version of base layer

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Input, Slider } from 'antd';
+import { TimerSlider, TimerInputs } from './timerInputs'
 
 
 export class Countdown extends React.Component {
@@ -7,69 +7,41 @@ export class Countdown extends React.Component {
         super(props)
         this.state = {
             disabled: false,
-            timerTime: {
-                secs: 0,
-                mins: 0,
-            },
+            timerTime: null,
         }
     }
 
-    sliderHolder = (value) => {
-        // console.log(value);
-        const mins = Math.floor(value/60)
-        const secs = value/60 > 0 ? value%60 : 0
-        this.setState({
-            timerTime: {
-                secs, mins
-            },
-        })
-    }
+    // sliderHolder = (value) => {
+    //     // console.log(value);
+    //     const mins = Math.floor(value/60)
+    //     const secs = value/60 > 0 ? value%60 : 0
+    //     this.setState({
+    //         timerTime: {
+    //             secs, mins
+    //         },
+    //     })
+    // }
 
-    minsHolder = ({target: {value}}) => {
+    // minsHolder = ({target: {value}}) => {
         
-        this.setState({ timerTime: { mins: value } });
-        console.log(this.state.timerTime)
-    }
+    //     this.setState({ timerTime: { mins: value } });
+    //     console.log(this.state.timerTime)
+    // }
 
-    secsHolder = ({target: {value}}) => {
+    // secsHolder = ({target: {value}}) => {
         
-        this.setState({ timerTime: { secs: value } });
-    }
+    //     this.setState({ timerTime: { secs: value } });
+    // }
 
 
 
     render() {
-        const { disabled, timerTime } = this.state;
+        const { disabled } = this.state;
         return(
             <div className="countdown">
-                <Slider defaultValue={30}
-                disabled={disabled}
-                step={15}
-                min={0}
-                max={3600}
-                value={timerTime ? timerTime.secs+ timerTime.mins*60 : null}
-                onChange={this.sliderHolder}
-                />
-                <div className="group">
-                    <label>
-                        Minutes
-                        <Input type="number"
-                        name="mins"
-                        disabled={disabled}
-                        placeholder="munutes"
-                        onChange={this.minsHolder}
-                        />
-                    </label>
-                    <label>
-                        seconds
-                        <Input type="number"
-                        name="secs"
-                        disabled={disabled}
-                        placeholder="seconds"
-                        onChange={this.secsHolder}
-                        />
-                    </label>
-                </div>
+                <span>Seconds</span>
+                <TimerSlider disabled={disabled}/>
+                <TimerInputs disabled={disabled}/>
             </div>
         )
 
